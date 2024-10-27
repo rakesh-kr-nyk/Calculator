d5117e16ba9bc01526e3424ccf39d1bea6cf638c
Rakesh nayak <iamrkjsr@gmail.com>
2024-10-27 19:38:00 +0530

    updated js file

--- index.js
+++ index.js
@@ -3,22 +3,25 @@ const buttons = document.querySelectorAll("button");
 let string = "";
 let arry = Array.from(buttons);
 
+// Add a click event listener to each button
 arry.forEach(button => {
     button.addEventListener("click", (e) => {
         if (e.target.innerText == "=") {
-                string = eval(string);
-                display.value = string;
+            string = eval(string); 
+            display.value = string; lt
+
         } else if (e.target.innerText == "AC") {
-            string = "";
-            display.value = string;
-        }
-        else if (e.target.innerText == "DEL") {
-            string = string.substring(0, string.length - 1);
-            display.value = string;
-        }
-        else {
-            string += e.target.innerText;
-            display.value = string;
+            string = ""; 
+            display.value = string; 
+
+        } else if (e.target.innerText == "DEL") {
+            string = string.substring(0, string.length - 1); 
+            display.value = string; 
+
+
+        } else {
+            string += e.target.innerText; 
+            display.value = string; 
         }
     });
 });
